9c87f0129dc07e686267d7d49e19fd39c6afa263
Kevin <kevinrivera.a4b@gmail.com>
2020-09-17 15:28:10 +0200

    .

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,17 @@
 import React from "react"
+import Header from '../components/Header/Header';
 import Banner from '../components/Banner/Banner';
-
+import Advantages from '../components/Advantages/Advantages';
+import ServiceFeatures from '../components/ServiceFeatures/ServiceFeatures';
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <>
        <SEO title="Diseño web rápido, original y seguro | MonkeyCode" />
+       <Header />
        <Banner />
+       <Advantages />
+       <ServiceFeatures/>
   </>
 )
 
